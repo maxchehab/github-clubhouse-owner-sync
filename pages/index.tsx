b19e3ec428a54933cb2ea7ecdd4d0902747a234e
maxchehab <maxchehab@gmail.com>
2020-01-10 02:55:44 +0000

    fix fetching

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,16 @@
 import { Component } from 'react';
-import { NextPageContext } from 'next';
+import { NextPageContext, NextApiRequest } from 'next';
 import { Pane } from 'evergreen-ui';
 import { parseCookies } from 'nookies';
 import axios, { AxiosError } from 'axios';
+import fetch from 'isomorphic-unfetch';
 
 import Avatar from '../components/avatar';
 import ClubhouseMember from '../common/interfaces/clubhouse-member.interface';
 import GitHubMember from '../common/interfaces/github-member.interface';
 import MemberTable from '../components/member-table';
 import Session from '../common/interfaces/session.interface';
+import { ServerRequest } from 'http';
 
 interface Props {
   clubhouseMembers: ClubhouseMember[];
@@ -16,10 +18,17 @@ interface Props {
   session: Session;
 }
 
+function getBaseURL({ headers }: ServerRequest): string {
+  const proto = process.env.NODE_ENV === 'production' ? 'https' : 'http';
+  const host = headers['x-forwarded-host'];
+
+  return `${proto}://${host}`;
+}
+
 export default class extends Component<Props> {
   static async getInitialProps(ctx: NextPageContext) {
     if (ctx.res) {
-      const baseURL = process.env.BASE_URL;
+      const baseURL = getBaseURL(ctx.req);
       const { sid } = parseCookies(ctx);
 
       if (!sid) {
@@ -28,37 +37,39 @@ export default class extends Component<Props> {
         });
         ctx.res.end();
       }
+
       try {
-        const sessionResponse = await axios.get<Session>(
-          `${baseURL}/api/session`,
-          {
-            headers: ctx.req.headers,
+        const sessionResponse = await fetch(`${baseURL}/api/session`, {
+          credentials: 'include',
+          headers: {
+            cookie: ctx.req.headers.cookie as string,
           },
-        );
+        });
 
-        const gitHubMembers = await axios.get<GitHubMember[]>(
-          `${baseURL}/api/github/members`,
-          {
-            headers: ctx.req.headers,
+        const gitHubMembers = await fetch(`${baseURL}/api/github/members`, {
+          credentials: 'include',
+          headers: {
+            cookie: ctx.req.headers.cookie as string,
           },
-        );
+        });
 
-        const clubhouseMembers = await axios.get<ClubhouseMember[]>(
+        const clubhouseMembers = await fetch(
           `${baseURL}/api/clubhouse/members`,
           {
-            headers: ctx.req.headers,
+            credentials: 'include',
+            headers: {
+              cookie: ctx.req.headers.cookie as string,
+            },
           },
         );
 
         return {
-          session: sessionResponse.data,
-          gitHubMembers: gitHubMembers.data,
-          clubhouseMembers: clubhouseMembers.data,
+          session: await sessionResponse.json(),
+          gitHubMembers: await gitHubMembers.json(),
+          clubhouseMembers: await clubhouseMembers.json(),
         };
       } catch (error) {
-        const { config, code, message } = error as AxiosError;
-
-        console.error({ config, code, message });
+        console.error(error);
 
         ctx.res.writeHead(302, {
           Location: '/login',
